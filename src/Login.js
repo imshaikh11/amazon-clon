3e75c09fad1b2024754d07a5963c304150b4cac3
ismail1011 <imshaikh1011@gmail.com>
2022-01-18 13:04:43 +0530

    new CSS changes

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react'
-import { Link , useHistory } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { auth } from './firebase';
 import './Login.css';
 import logo from "./Images/logo2.png";
+import profile from "./Images/user-logo.png";
 
 function Login() {
 
@@ -13,16 +14,16 @@ function Login() {
     const signIn = e => {
         e.preventDefault();
 
-        auth 
+        auth
             .signInWithEmailAndPassword(email, password)
-            .then((auth)  => {
+            .then((auth) => {
                 //it successfully signIn a user with Email and Password 
-                if(auth){
+                if (auth) {
                     history.push('/')
-                }   
-                
+                }
+
             }
-                
+
             )
             .catch(error => alert(error.message))
 
@@ -31,15 +32,15 @@ function Login() {
 
     const register = e => {
         e.preventDefault();
-        
+
         auth
             .createUserWithEmailAndPassword(email, password)
-            .then((auth)  => {
+            .then((auth) => {
                 //it successfully created a new user with Email and Password 
-                if(auth){
+                if (auth) {
                     history.push('/')
-                }   
-                
+                }
+
             })
             .catch(error => alert(error.message))
 
@@ -47,26 +48,26 @@ function Login() {
 
     return (
         <div className='login'>
-            <Link to = '/'>
-                <img className="login__logo" src={logo} alt='logo'/>
-            </Link> 
+            <Link to='/'>
+                <img className="login__logo" src={logo} alt='logo' />
+            </Link>
 
             <div className='login__container'>
+                <img className="login__profile" src={profile} alt="profile" />
                 <h1>Sign In</h1>
                 <form>
                     <h5>E-mail</h5>
-                    <input type='text' value={email} onChange={e => setEmail(e.target.value)}/>
+                    <input type='text' placeholder='  Enter Your E-mail ID' value={email} onChange={e => setEmail(e.target.value)} />
                     <h5>Password</h5>
-                    <input type='password' value={password} onChange={e => setPassword(e.target.value)}/>
-                    <button type ='submit' onClick={signIn} className='login__signButton'>Sign In</button>
+                    <input type='password' placeholder='  Enter Your Password' value={password} onChange={e => setPassword(e.target.value)} />
+                    <button type='submit' onClick={signIn} className='login__signButton'>Sign In</button>
                 </form>
+                <a href="#">Forgot Password?</a>
                 <p className='login__info'>
                     By continuing, you agree to Amazon's Conditions of Use and Privacy Notice.
                 </p>
-                <a href="#">Forgot Password?</a>
                 <button type='submit' onClick={register} className='login__registerButton'>Create Your Amazon Account</button>
             </div>
-
         </div>
     )
 }
